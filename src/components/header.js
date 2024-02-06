f06f27936b3c003dc84676eec92163e844249b59
Niharikamillu <97603523+Niharikamillu@users.noreply.github.com>
2024-02-06 14:11:00 +0530

    created header

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function header() {
+
+    return (
+        
+            <>
+            <img src="http://localhost:3000/school.jpg" class="Logo-size" alt="logo"></img>
+            <h1 className='headerbar'>Archery Montessori School </h1>
+            <img src="http://localhost:3000/logos1.jpg" class="Logo-size" alt="logo"></img>
+            </>
+        
+    );
+}
+export default header;
